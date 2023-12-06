6b559c980c9a7c93d863346ee14ce54a1a7cb54c
Kaspryk <Will.Kaspryk@gov.bc.ca>
2023-12-05 16:32:32 -0800

    some initial styling and structure for the global header

--- src/dashboard/src/components/auth/AuthState.tsx
+++ src/dashboard/src/components/auth/AuthState.tsx
@@ -10,25 +10,21 @@ export const AuthState = () => {
   if (session.status === 'loading') return <div>loading...</div>;
   else if (session.status === 'authenticated') {
     return (
-      <div>
         <button onClick={() => keycloakSessionLogOut()} title="Sign out">
           <div className="group flex flex-row gap-1 items-center text-gray-100 hover:text-gray-500 active:text-gray-900">
             <FaSignOutAlt className="fill-gray-100 group-hover:fill-gray-500 group-active:fill-gray-900" />
             Sign out
           </div>
         </button>
-      </div>
     );
   }
 
   return (
-    <div>
       <button onClick={() => signIn('keycloak')} title="Sign in">
-        <div className="group flex flex-row gap-1 items-center text-gray-100 hover:text-gray-500 active:text-gray-900">
+        <div className="group flex flex-row gap-1 items-center text-gray-100 hover:text-gray-500 active:text-gray-900 font-bold">
           <FaSignInAlt className="fill-gray-100 hover:fill-gray-500 active:fill-gray-900" />
           Sign in
         </div>
       </button>
-    </div>
   );
 };
--- src/dashboard/src/components/header/Header.tsx
+++ src/dashboard/src/components/header/Header.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AuthState } from '../auth';
-import './style.css';
+import Image from 'next/image';
+import style from './Header.module.scss';
 
 export const Header: React.FC = () => {
   const path = usePathname();
@@ -12,14 +13,25 @@ export const Header: React.FC = () => {
   const isLogin = path.includes('/login');
 
   return (
-    <header className="header">
-      <div className="flex-1 flex flex-row gap-4">
-        <Link href="/">
+    <header className={style.header}>
+      <div className={style.container}>
+        <div className={style.headerTop}>
+          <div>
           <Image src="/images/BCLogo.png" alt="Logo" width={215} height={90} />
-        </Link>
-        <div className="border-l-2 p-2 text-2xl font-light">Storage Dashboard</div>
+          <h1>Storage Dashboard</h1>
+          </div>
+          {!isLogin && <AuthState />}
+        </div>
+        <div className={style.headerMiddle}>
+          <p>Welcome to the storage dashboard.  This is an overview of the storage consumption for all organizations you belong to.  Use the filters to see further breakdowns of storage data.</p>
+        </div>
+        <div className={style.headerBottom}>
+          <nav>
+            <a href="" className={style.active}>Storage</a>
+            <a href="">Administration</a>
+          </nav>
+        </div>
       </div>
-      {!isLogin && <AuthState />}
     </header>
   );
 };
