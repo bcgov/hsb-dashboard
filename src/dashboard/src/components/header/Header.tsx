1c4aa281999457a05a03f059605123303542b3d2
wkaspryk <wkaspryk@gmail.com>
2023-12-07 17:26:09 -0800

    HOSTSD-169 - Adding header component (#19)
    * some initial styling and structure for the global header
    
    * more styles and structure for header, adding admin nav
    
    * adding admin nav sub menu, adding some variables and mixins
    
    * adding extra conditional check for header sub-navs
    
    * adding all servers link to header
    
    * cleaning up header imports after rebase

--- src/dashboard/src/components/header/Header.tsx
+++ src/dashboard/src/components/header/Header.tsx
@@ -4,22 +4,60 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AuthState } from '../auth';
-import './style.css';
+import style from './Header.module.scss';
 
 export const Header: React.FC = () => {
   const path = usePathname();
 
   const isLogin = path.includes('/login');
 
+  const isAdmin = true;
+  const isHSBAdmin = true;
+  const isHSBAdminOrganizations = true;
+  const infoIcon = false;
+
   return (
-    <header className="header">
-      <div className="flex-1 flex flex-row gap-4">
-        <Link href="/">
-          <Image src="/images/BCLogo.png" alt="Logo" width={215} height={90} />
-        </Link>
-        <div className="border-l-2 p-2 text-2xl font-light">Storage Dashboard</div>
+    <header className={style.header}>
+      <div className={style.container}>
+        <div className={style.headerTop}>
+          <div>
+            <Link href="">
+              <Image src="/images/BCLogo.png" alt="Logo" width={134} height={56} />
+            </Link>
+            <h1>Storage Dashboard</h1>
+          </div>
+          {!isLogin && <AuthState />}
+        </div>
+        <div className={style.headerMiddle}>
+          {infoIcon && <span className={style.infoIcon}></span>}
+          <p>Welcome to the storage dashboard.  This is an overview of the storage consumption for all organizations you belong to.  <br/>Use the filters to see further breakdowns of storage data.</p>
+        </div>
+        <div className={style.headerBottom}>
+          <nav>
+            <a href="" className={`${!isAdmin && style.active} ${style.storage}`}>Storage</a>
+            <a href="" className={`${isAdmin && style.active} ${style.admin} `}>Administration</a>
+          </nav>
+          {!isAdmin && <a href="" className={style.allServers}>See all servers</a>}
+          {isAdmin && isHSBAdmin && ( 
+            <>
+              <nav>
+                <div className={style.adminNav}>
+                  <a href="" title="Look up organizations and enable them on dashboard" className={style.active}>Organizations</a>
+                  <a href="" title="Manage user access to dashboard, assign roles">Users</a>
+                </div>
+              </nav>
+              <span className={style.navLine}></span>
+            </>
+          )}
+          {isAdmin && isHSBAdmin && isHSBAdminOrganizations && (
+            <nav className={style.adminSubNav}>
+                <a href="" className={`${style.subNavItem} ${style.active}`}>All Organizations</a>
+                <a href="" className={`${style.subNavItem}`}>Add New Organization</a>
+                <a href="" className={`${style.subNavItem}`}>Update Existing Organization</a>
+            </nav>
+          )}
+        </div>
       </div>
-      {!isLogin && <AuthState />}
     </header>
   );
 };
