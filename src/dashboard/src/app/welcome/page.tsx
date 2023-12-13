96b0a378ecf6e0c5e81c3e9c7b4bce487dbffed0
Kaspryk <Will.Kaspryk@gov.bc.ca>
2023-12-13 13:24:36 -0800

    adding styling for login and welcome pages

--- src/dashboard/src/app/welcome/page.tsx
+++ src/dashboard/src/app/welcome/page.tsx
@@ -1,3 +1,16 @@
+import styles from './Welcome.module.scss';
+
 export default function Page() {
-  return <div>Welcome</div>;
+  return (
+    <div className={`dashboardContainer ${styles.container}`}>
+      <div className={styles.welcome}>
+        <h2>Request Access</h2>
+        <p>Please email <a href="mailto:placeholder@gov.bc.ca">placeholder@gov.bc.ca</a> to request access to your organization's dashboard</p>
+      </div>
+      <div className={styles.login}>
+        <h2>Welcome to the Storage Dashboard</h2>
+        <p>Sign in to get insights into your organization’s data storage and usage.</p>
+      </div>
+    </div>
+  );
 }
--- src/dashboard/src/components/header/Header.tsx
+++ src/dashboard/src/components/header/Header.tsx
@@ -39,7 +39,7 @@ export const Header: React.FC = () => {
     <>
       <header className={`${style.header} ${isClient && isDashboardView && style.filterPadding}`}>
         <div className={style.container}>
-          <div className={style.headerTop}>
+          <div className={`${(path.startsWith(`/login`) || path.startsWith(`/welcome`)) && style.login} ${style.headerTop}`}>
             <div>
               <Link href="/">
                 <Image src="/images/BCLogo.png" alt="Logo" width={134} height={56} />
@@ -48,104 +48,109 @@ export const Header: React.FC = () => {
             </div>
             {!isLogin && <AuthState showName={true} />}
           </div>
-          <div className={style.headerMiddle}>
-            {infoIcon && <span className={style.infoIcon}></span>}
-            <p>
-              Welcome to the storage dashboard. This is an overview of the storage consumption for
-              all organizations you belong to. <br />
-              Use the filters to see further breakdowns of storage data.
-            </p>
-          </div>
-          <div className={style.headerBottom}>
-            {isAuthorized && (
-              <>
-                {(isClient || isHSB) && (
+          {!(path.includes(`/login`) || path.includes(`/welcome`)) && (
+            <>
+              <div className={style.headerMiddle}>
+                {infoIcon && <span className={style.infoIcon}></span>}
+                <p>
+                  Welcome to the storage dashboard. This is an overview of the storage consumption
+                  for all organizations you belong to. <br />
+                  Use the filters to see further breakdowns of storage data.
+                </p>
+              </div>
+              <div className={style.headerBottom}>
+                {isAuthorized && (
                   <>
-                    <nav>
-                      <Link
-                        href={`/${rootPath}/dashboard`}
-                        className={`${path.startsWith(`/${rootPath}/dashboard`) && style.active} ${
-                          style.storage
-                        }`}
-                      >
-                        Storage
-                      </Link>
-                      {(isOrganizationAdmin || isSystemAdmin) && (
-                        <Link
-                          href={`/${rootPath}/admin`}
-                          className={`${path.startsWith(`/${rootPath}/admin`) && style.active} ${
-                            style.admin
-                          } `}
-                        >
-                          Administration
-                        </Link>
-                      )}
-                    </nav>
-                    {isDashboardView && (
-                      <Link href="/client/dashboard" className={style.allServers}>
-                      See all servers
-                    </Link>
-                    )}
-                    {isSystemAdmin && !isDashboardView && (
+                    {(isClient || isHSB) && (
                       <>
                         <nav>
-                          <div className={style.adminNav}>
+                          <Link
+                            href={`/${rootPath}/dashboard`}
+                            className={`${
+                              path.startsWith(`/${rootPath}/dashboard`) && style.active
+                            } ${style.storage}`}
+                          >
+                            Storage
+                          </Link>
+                          {(isOrganizationAdmin || isSystemAdmin) && (
                             <Link
-                              href={`/${rootPath}/admin/organizations`}
-                              title="Look up organizations and enable them on dashboard"
+                              href={`/${rootPath}/admin`}
                               className={`${
-                                path.startsWith(`/${rootPath}/admin/organizations`) && style.active
-                              }`}
+                                path.startsWith(`/${rootPath}/admin`) && style.active
+                              } ${style.admin} `}
                             >
-                              Organizations
+                              Administration
                             </Link>
-                            <Link
-                              href={`/${rootPath}/admin/users`}
-                              title="Manage user access to dashboard, assign roles"
-                              className={`${
-                                path.startsWith(`/${rootPath}/admin/users`) && style.active
-                              }`}
-                            >
-                              Users
-                            </Link>
-                          </div>
+                          )}
                         </nav>
-                        <span className={style.navLine}></span>
+                        {isDashboardView && (
+                          <Link href="/client/dashboard" className={style.allServers}>
+                            See all servers
+                          </Link>
+                        )}
+                        {isSystemAdmin && !isDashboardView && (
+                          <>
+                            <nav>
+                              <div className={style.adminNav}>
+                                <Link
+                                  href={`/${rootPath}/admin/organizations`}
+                                  title="Look up organizations and enable them on dashboard"
+                                  className={`${
+                                    path.startsWith(`/${rootPath}/admin/organizations`) &&
+                                    style.active
+                                  }`}
+                                >
+                                  Organizations
+                                </Link>
+                                <Link
+                                  href={`/${rootPath}/admin/users`}
+                                  title="Manage user access to dashboard, assign roles"
+                                  className={`${
+                                    path.startsWith(`/${rootPath}/admin/users`) && style.active
+                                  }`}
+                                >
+                                  Users
+                                </Link>
+                              </div>
+                            </nav>
+                            <span className={style.navLine}></span>
+                          </>
+                        )}
                       </>
+                    )}
+                    {isSystemAdmin && path.startsWith('/hsb/admin/organizations') && (
+                      <nav className={style.adminSubNav}>
+                        <Link
+                          href="/hsb/admin/organizations"
+                          className={`${style.subNavItem} ${
+                            path === '/hsb/admin/organizations' && style.active
+                          }`}
+                        >
+                          All Organizations
+                        </Link>
+                        <Link
+                          href="/hsb/admin/organizations/0"
+                          className={`${style.subNavItem} ${
+                            path.startsWith('/hsb/admin/organizations/0') && style.active
+                          }`}
+                        >
+                          Add New Organization
+                        </Link>
+                        <Link
+                          href="/hsb/admin/organizations/1"
+                          className={`${style.subNavItem} ${
+                            /\/hsb\/admin\/organizations\/[1-9]+/.test(path) && style.active
+                          }`}
+                        >
+                          Update Existing Organization
+                        </Link>
+                      </nav>
                     )}
                   </>
                 )}
-                {isSystemAdmin && path.startsWith('/hsb/admin/organizations') && (
-                  <nav className={style.adminSubNav}>
-                    <Link
-                      href="/hsb/admin/organizations"
-                      className={`${style.subNavItem} ${
-                        path === '/hsb/admin/organizations' && style.active
-                      }`}
-                    >
-                      All Organizations
-                    </Link>
-                    <Link
-                      href="/hsb/admin/organizations/0"
-                      className={`${style.subNavItem} ${
-                        path.startsWith('/hsb/admin/organizations/0') && style.active
-                      }`}
-                    >
-                      Add New Organization
-                    </Link>
-                    <Link
-                      href="/hsb/admin/organizations/1"
-                      className={`${style.subNavItem} ${
-                        /\/hsb\/admin\/organizations\/[1-9]+/.test(path) && style.active
-                      }`}
-                    >
-                      Update Existing Organization
-                    </Link>
-                  </nav>
-                )}
-              </>
-            )}
-          </div>
+              </div>
+            </>
+          )}
         </div>
       </header>
       {(isClient || isHSB) && isDashboardView && <Filter />}
