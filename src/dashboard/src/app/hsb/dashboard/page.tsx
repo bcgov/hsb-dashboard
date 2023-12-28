0f13209d65248a0c8550c5f9704b12e85368d9c0
Kaspryk <Will.Kaspryk@gov.bc.ca>
2023-12-28 09:40:37 -0800

    moving dashboardContainer to layout after rebase

--- src/dashboard/src/app/hsb/dashboard/page.tsx
+++ src/dashboard/src/app/hsb/dashboard/page.tsx
@@ -1,13 +1,10 @@
-'use client';
-
 import { AllOrgDonutChart } from '@/components/allOrgDonutChart';
-import { Sheet } from '@/components';
 
 export default function Page() {
   return (
-    <div className="dashboardContainer">
+    <>
       HSB Dashboard
       <AllOrgDonutChart />
-    </div>
+    </>
   );
 }
