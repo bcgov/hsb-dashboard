0e2bb7e09ff737ea92f7d9d42962461d64051249
Frank Hangler <frank@plotandscatter.com>
2024-12-10 14:09:50 -0800

    Change how redirects work based on roles

--- src/dashboard/src/app/page.tsx
+++ src/dashboard/src/app/page.tsx
@@ -5,12 +5,17 @@ import { useAuth } from '@/hooks';
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-  const { isClient, isHSB, isAuthorized, session } = useAuth();
+  const { isClient, isHSB, isAuthorized, isSystemAdmin, session } = useAuth();
 
   // Redirect to default page for each type of user.
   if (isClient) redirect('/client/dashboard');
   else if (isHSB) redirect('/hsb/dashboard');
-  else if (isAuthorized) redirect('/welcome');
+  else if (isSystemAdmin) redirect('/hsb/admin/users');
+  else if (isAuthorized) {
+    // In this case, although authorized, the user does not have the roles we expect, so we have to
+    // treat them as if they are unauthorized.
+    redirect('/not-authorized');
+  }
 
   return (
     <main>
