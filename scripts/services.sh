4f500ed6a78258381eb9f4d927b9f99b0eee14b6
Frank Hangler <frank@plotandscatter.com>
2025-02-05 13:59:33 -0800

    Enable cacheing of apt-get etc.

--- scripts/services.sh
+++ scripts/services.sh
@@ -7,7 +7,8 @@ data-service () {
   if [[ "$(docker inspect hsb-data-service > /dev/null 2>&1 && echo 'yes' || echo 'no')" == "yes" ]]; then
     docker rm hsb-data-service -f
   fi
-  docker build -t hsb:data-service -f src/data-service/Dockerfile . --no-cache --force-rm
+  # ORIGINAL: docker build -t hsb:data-service -f src/data-service/Dockerfile . --no-cache --force-rm
+  docker build -t hsb:data-service -f src/data-service/Dockerfile . --force-rm
   docker run -i --env-file=src/data-service/.env --name hsb-data-service hsb:data-service
   docker rm hsb-data-service -f
   docker image rm hsb:data-service -f
