5ddd8ca3ad457bf2632a85e0696207cc3927283c
Frank Hangler <frank@plotandscatter.com>
2025-03-14 13:56:44 -0700

    Toggle switch (SAN / non-SAN) placeholder

--- src/dashboard/src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/dashboard/src/components/breadcrumbs/Breadcrumbs.tsx
@@ -2,42 +2,48 @@ import styles from './Breadcrumbs.module.scss';
 import { useDashboardStore } from '@/store';
 
 interface BreadcrumbsProps {
-    multipleOrganizations: boolean;
+  multipleOrganizations: boolean;
 }
 
 export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ multipleOrganizations }) => {
-  const {
-    organization,
-    operatingSystemItem,
-    serverItem,
-  } = useDashboardStore(state => ({
+  const { organization, operatingSystemItem, serverItem } = useDashboardStore((state) => ({
     organization: state.organization,
     operatingSystemItem: state.operatingSystemItem,
     serverItem: state.serverItem,
   }));
 
   return (
-    <div className={styles.breadcrumbs}>
-       {!!multipleOrganizations && (
-        <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+      }}
+    >
+      <div className={styles.breadcrumbs}>
+        {!!multipleOrganizations && (
+          <div>
             <p>All Organizations</p>
-        </div>
-       )}
-      {!!organization && (
-        <div>
-          <p title={organization.name}>{organization.name}</p>
-        </div>
-      )}
-      {!!operatingSystemItem && (
-        <div>
-          <p title={operatingSystemItem.name}>{operatingSystemItem.name}</p>
-        </div>
-      )}
-      {!!serverItem && (
-        <div>
-          <p title={serverItem.name}>{serverItem.name}</p>
-        </div>
-      )}
+          </div>
+        )}
+        {!!organization && (
+          <div>
+            <p title={organization.name}>{organization.name}</p>
+          </div>
+        )}
+        {!!operatingSystemItem && (
+          <div>
+            <p title={operatingSystemItem.name}>{operatingSystemItem.name}</p>
+          </div>
+        )}
+        {!!serverItem && (
+          <div>
+            <p title={serverItem.name}>{serverItem.name}</p>
+          </div>
+        )}
+      </div>
+      <div style={{ width: '60px', marginBottom: '20px' }}>Toggle switch</div>
     </div>
   );
 };
