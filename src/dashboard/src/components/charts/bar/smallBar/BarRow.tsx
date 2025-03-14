ed1fd6600c60f8ad7a7deac4cfa773375f3da1c4
Frank Hangler <frank@plotandscatter.com>
2025-03-14 13:58:54 -0700

    Show SAN / non-SAN label. WIP

--- src/dashboard/src/components/charts/bar/smallBar/BarRow.tsx
+++ src/dashboard/src/components/charts/bar/smallBar/BarRow.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { convertToStorageSize } from './../../../../utils/convertToStorageSize';
 import { IBarChartRowData } from './IBarChartRowData';
 import styles from './SmallBarChart.module.scss';
+import { IFileSystemItemModel, IServerItemModel } from '@/hooks';
 
 interface IBarRowProps extends Omit<IBarChartRowData<unknown>, 'label'> {
   label: React.ReactNode;
@@ -12,17 +13,46 @@ interface IBarRowProps extends Omit<IBarChartRowData<unknown>, 'label'> {
  * @param param0 Component properties
  * @returns Component
  */
-export const BarRow: React.FC<IBarRowProps> = ({ label, capacity, available }) => {
+export const BarRow: React.FC<IBarRowProps> = (props) => {
+  const { label, capacity, available } = props;
+
   const used = capacity - available;
   const percentageUsed = capacity ? Math.round((used / capacity) * 100) : 0;
   const capacityValue = convertToStorageSize(capacity, 'B', 'TB');
   const usedValue = convertToStorageSize(used, 'B', 'TB');
   const availableValue = convertToStorageSize(available, 'B', 'TB');
 
+  const showDiskType = (props.data as IFileSystemItemModel)?.isSAN !== undefined;
+
+  const diskType = showDiskType
+    ? (props.data as IFileSystemItemModel)?.isSAN
+      ? 'SAN'
+      : 'Non-SAN'
+    : '';
+
   return (
     <div className={styles.row}>
       <div className={styles.info}>
-        {label}
+        <p style={{ display: 'flex', alignItems: 'center' }}>
+          {showDiskType && (
+            <span className={diskType === 'SAN' ? styles.badgeYellow : styles.badge}>
+              {diskType}
+            </span>
+          )}
+          {label}{' '}
+          <div
+            style={{
+              fontSize: '12px',
+              marginLeft: '10px',
+              border: 'solid 1px #999',
+              borderRadius: '4px',
+              padding: '1px 3px',
+              color: '#999',
+            }}
+          >
+            {props.data?.storageType}
+          </div>
+        </p>
         <p>{capacityValue}</p>
         <p>{usedValue}</p>
         <p>{availableValue}</p>
